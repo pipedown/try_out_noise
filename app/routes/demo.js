0a53e59555fa08232a14f952e9a5ed21bf80c78f
Volker Mische <volker.mische@gmail.com>
2017-05-07 16:51:27 +0200

    Add basic pager

--- app/routes/demo.js
+++ app/routes/demo.js
@@ -4,21 +4,30 @@ export default Ember.Route.extend({
   model(params) {
     const tutorial = this.modelFor('application');
     const pageNo = parseInt(params.demo_page, 10);
+
+    let next = null;
+    if (pageNo + 1 < tutorial.descriptions.length) {
+      next = {
+        title: tutorial.titles[pageNo + 1],
+        pageNo: pageNo + 1
+      }
+    }
+
+    let prev = null;
+    if (pageNo - 1 > 0) {
+      prev = {
+        title: tutorial.titles[pageNo - 1],
+        pageNo: pageNo - 1
+      }
+    }
+
     return {
       current: {
         query: tutorial.queries[pageNo],
         description: tutorial.descriptions[pageNo],
       },
-      // TODO vmx 2017-05-06: Only define it if there's a next step
-      next: {
-        title: tutorial.titles[pageNo + 1],
-        pageNo: pageNo + 1
-      },
-      // TODO vmx 2017-05-06: Only define it if there's a previous step
-      prev: {
-        title: tutorial.titles[pageNo - 1],
-        pageNo: pageNo - 1
-      }
+      next,
+      prev
     };
   }
 });
