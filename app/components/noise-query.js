0df2efd049478a4c182d0a44833315b834839c2f
Volker Mische <volker.mische@gmail.com>
2017-05-08 14:40:28 +0200

    Empty the results when changing the tutorial page

--- app/components/noise-query.js
+++ app/components/noise-query.js
@@ -2,12 +2,14 @@ import Ember from 'ember';
 import fetch from "ember-network/fetch";
 
 export default Ember.Component.extend({
-  tagName: '',
-
   results: null,
+  // Store the query that was set by the tutorial. If that query changes,
+  // by going to another tutorial step, then empty the results.
+  tutorialQuery: null,
 
   actions: {
-    doQuery(query) {
+    doQuery() {
+      const query = this.$('textarea').val();
       fetch("http://127.0.0.1:3000/query", {
         method: "POST",
         body: query
@@ -17,5 +19,14 @@ export default Ember.Component.extend({
         this.set('results', json);
       });
     }
-  }
+  },
+
+  didUpdateAttrs() {
+    // Empty the results only if the query changed to due to the tutorial,
+    // don't empty them when the user does some input.
+    if (this.get('tutorialQuery') != this.get('attrs').query.value) {
+      this.set('results', null);
+      this.set('tutorialQuery', this.get('attrs').query.value);
+    }
+  },
 });
